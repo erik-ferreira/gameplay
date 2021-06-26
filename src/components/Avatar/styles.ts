8dac3a3309bbb2c8d2281bd3d9d1d9d97dae5c57
erik-ferreira <erikdesouzagf@gmail.com>
2021-06-26 13:10:29 -0300

    component avatar created

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    width: 50,
+    height: 50,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 22,
+  },
+
+  avatar: {
+    width: 46,
+    height: 46,
+    borderRadius: 8,
+  },
+});
+
+export default styles;
